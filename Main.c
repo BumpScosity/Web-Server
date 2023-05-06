2ed48196382491868baff6fb3aaecc7d5a96a928
BumpScosity <BumpScosity@outlook.com>
2023-05-06 23:50:59 +0300

    Fixed Main.c (hopefully)

--- Main.c
+++ Main.c
@@ -1,45 +1,43 @@
 #include "lib/Main.h"
-#include <stdio.h>
 
 int main() {
-    printf("1");
-    signals signal[2];
+    pid_t pid;
+    signals signal[1];
     signal[0].exit = 0;
     signal[0].close = 0;
-    pid_t pid = fork();
-    printf("2");
 
+    pid = fork();
     if (pid == 0) {
-        // Child process runs the server
-        run_server(signal);
-        printf("3");
-    } else {
-        // Parent process waits for input from user
-        char command[100];
-        printf("4");
+        // Child process
+        run_server();
+        exit(0);
+    } else if (pid > 0) {
+        // Parent process
         while (1) {
             if (signal[0].exit == 1) {
-                printf("5");
                 kill(pid, SIGTERM);
                 wait(NULL);
                 exit(0);
             }
             else {
-                printf("6");
-                printf("(server) ");
-                fgets(command, sizeof(command), stdin);
-                command[strlen(command) - 1] = '\0';  // remove newline character
-            
+                char command[100];
+                printf("Enter a command: ");
+                scanf("%s", command);
                 if (strcmp(command, "stop") == 0) {
-                    signal[1].close = 1;
+                    signal[0].close = 1;
                     break;
-                } else {
-                    printf("Unknown command, use \"help\" to get a list of commands.\n");
                 }
             }
         }
+
+        // Wait for the child process to exit before terminating
+        int status;
+        waitpid(pid, &status, 0);
+    } else {
+        // Error forking
+        printf("Error: Could not fork.\n");
+        exit(1);
     }
 
     return 0;
 }
-
