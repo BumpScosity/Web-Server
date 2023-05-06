cc60381834ec8caf20cfa1153362f6d0afb103a7
BumpScosity <BumpScosity@outlook.com>
2023-05-06 23:42:26 +0300

    Testing main

--- Main.c
+++ Main.c
@@ -1,24 +1,31 @@
 #include "lib/Main.h"
+#include <stdio.h>
 
 int main() {
+    printf("1");
     signals signal[2];
     signal[0].exit = 0;
     signal[0].close = 0;
     pid_t pid = fork();
+    printf("2");
 
     if (pid == 0) {
         // Child process runs the server
         run_server(signal);
+        printf("3");
     } else {
         // Parent process waits for input from user
         char command[100];
+        printf("4");
         while (1) {
             if (signal[0].exit == 1) {
+                printf("5");
                 kill(pid, SIGTERM);
                 wait(NULL);
                 exit(0);
             }
             else {
+                printf("6");
                 printf("(server) ");
                 fgets(command, sizeof(command), stdin);
                 command[strlen(command) - 1] = '\0';  // remove newline character
