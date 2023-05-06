5e7f40b3bb7a7485959e9f9276498c553036bfee
BumpScosity <BumpScosity@outlook.com>
2023-05-06 22:37:16 +0300

    Updated header files, again.

--- lib/Main.h
+++ lib/Main.h
@@ -13,6 +13,23 @@
 
 #include "Server.h"
 
+const double Codes[] = {
+    0, // File failed to open
+
+    1.1, // Invalid size format
+    1.2, // Size not found
+
+    2.1, // Invalid port format
+    2.2, // Port not found
+    2.3, // Invalid port number
+
+    3.1, // Invalid thread format
+    3.2, // Thread not found
+
+    4.1, // Invalid buffer format
+    4.2 // Buffer not found
+};
+
 typedef struct {
     int port;
     int threads;
