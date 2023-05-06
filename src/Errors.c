9c3c09de3bd828e46637762c5389f98eb3ae956f
BumpScosity <BumpScosity@outlook.com>
2023-05-06 23:22:33 +0300

    Fixed stuff

--- src/Errors.c
+++ src/Errors.c
@@ -1,5 +1,22 @@
 #include "../lib/Main.h"
 
+const double Codes[] = {
+    0, // File failed to open
+
+    1.1, // Invalid size format
+    1.2, // Size not found
+
+    2.1, // Invalid port format
+    2.2, // Port not found
+    2.3, // Invalid port number
+
+    3.1, // Invalid thread format
+    3.2, // Thread not found
+
+    4.1, // Invalid buffer format
+    4.2 // Buffer not found
+};
+
 void error_handle(int ErrorCode) {
     switch (ErrorCode) {
         case 0:
