9ca6f553c3669b95f0d21e09c43a5d3e463a3494
BumpScosity <BumpScosity@outlook.com>
2023-05-07 06:50:33 +0300

    Updates.

--- Main.c
+++ Main.c
@@ -1,65 +1,24 @@
 #include "lib/Main.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char** argv) {
     pid_t pid;
 
-    // Fork a child process
     pid = fork();
     if (pid == -1) {
-        perror("fork");
+        perror("fork error");
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
-        // This is the child process
-
-        // Run the server
+        // child process
         run_server();
-
-        // Exit the child process
         exit(EXIT_SUCCESS);
     } else {
-        // This is the parent process
+        // parent process
+        printf("Child process ID: %d\n", pid);
 
-        // Wait for a command
-        while (1) {
-            printf("Enter a command: ");
-            char command[100];
-            if (fgets(command, sizeof(command), stdin) == NULL) {
-                // If fgets returns NULL, an error occurred or the end of the file was reached
-                if (feof(stdin)) {
-                    printf("End of input reached\n");
-                    break;
-                } else {
-                    perror("fgets");
-                    exit(EXIT_FAILURE);
-                }
-            }
-
-            // Strip the newline character from the end of the command
-            int len = strlen(command);
-            if (len > 0 && command[len-1] == '\n') {
-                command[len-1] = '\0';
-            }
-
-            // Check the command
-            if (strcmp(command, "stop") == 0) {
-                // Send a SIGINT signal to the child process to stop the server
-                if (kill(pid, SIGINT) == -1) {
-                    perror("kill");
-                    exit(EXIT_FAILURE);
-                }
-
-                // Wait for the child process to exit
-                int status;
-                if (waitpid(pid, &status, 0) == -1) {
-                    perror("waitpid");
-                    exit(EXIT_FAILURE);
-                }
-
-                break;
-            } else {
-                printf("Unknown command: %s\n", command);
-            }
-        }
+        // wait for child process to exit
+        int status;
+        waitpid(pid, &status, 0);
+        printf("Child process exited with status %d\n", status);
     }
 
     return 0;
