85b20dd4fa0f380d6791e2fc4b522fc1b541089e
BumpScosity <BumpScosity@outlook.com>
2023-05-07 16:30:23 +0300

    Updates.

--- Main.c
+++ Main.c
@@ -1,38 +1,40 @@
 #include "lib/Main.h"
-#include <stdio.h>
 
-int main(int argc, char** argv) {
+int main() {
+    int pipefd[2];
     pid_t pid;
+    char buffer[1024];
+
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
 
     pid = fork();
+
     if (pid == -1) {
-        perror("fork error");
+        perror("fork");
         exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        // child process
-        printf("1");
-        run_server();
-        printf("2");
+    }
+
+    if (pid == 0) { // Child process
+        close(pipefd[0]); // Close the read end of the pipe
+
+        const char* message = "Hello, parent process!";
+        write(pipefd[1], message, strlen(message) + 1); // Write the message to the pipe
+
+        close(pipefd[1]); // Close the write end of the pipe
+
         exit(EXIT_SUCCESS);
-        printf("3");
-    } else {
-        // parent process
-        printf("Child process ID: %d\n", pid);
-        char command[100];
-        while(1) {
-            printf("(server) ");
-            fgets(command, sizeof(command), stdin);
-            command[strlen(command) - 1] = '\0';  // remove newline character
-            
-            if (strcmp(command, "stop") == 0) {
-                break;
-            } else {
-                printf("Unknown command\n");
-            }
-        }
-        int status;
-        waitpid(pid, &status, 0);
-        printf("Child process exited with status %d\n", status);
+    } else { // Parent process
+        close(pipefd[1]); // Close the write end of the pipe
+
+        read(pipefd[0], buffer, BUFFER_SIZE); // Read the message from the pipe
+        printf("Received message from child process: %s\n", buffer);
+
+        close(pipefd[0]); // Close the read end of the pipe
+
+        exit(EXIT_SUCCESS);
     }
 
     return 0;
