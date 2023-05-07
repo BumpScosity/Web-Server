afb50c2e5a6e9d55f48333f0d982606aa08da86c
BumpScosity <BumpScosity@outlook.com>
2023-05-07 16:41:28 +0300

    Updates.

--- Main.c
+++ Main.c
@@ -1,56 +1,69 @@
 #include "lib/Main.h"
 
-void child_process(int read_pipe, int write_pipe) {
-    // Read the function pointer from the pipe
-    void (*func)(void);
-    read(read_pipe, &func, sizeof(func));
+void child_process(int fd)
+{
+    char buf[1024];
+    int n;
 
-    // Call the function
-    run_server();
+    // Read command from parent process
+    while ((n = read(fd, buf, sizeof(buf))) > 0) {
+        // Null terminate the command string
+        buf[n] = '\0';
 
-    // Close the pipes and exit
-    close(read_pipe);
-    close(write_pipe);
+        // Execute the command function
+        if (strcmp(buf, "print_hello") == 0) {
+            printf("Hello from child process!\n");
+        } else if (strcmp(buf, "exit") == 0) {
+            break;
+        } else {
+            printf("Unknown command: %s\n", buf);
+        }
+    }
+
+    // Close the pipe and exit
+    close(fd);
     exit(0);
 }
 
-int main() {
-    int pipefd[2];
-    char buffer[1024];
+int main()
+{
+    int fd[2];
     pid_t pid;
 
-    // Create a pipe for communication between the parent and child processes
-    if (pipe(pipefd) == -1) {
+    // Create pipe
+    if (pipe(fd) == -1) {
         perror("pipe");
         exit(1);
     }
 
-    // Fork the process
+    // Create child process
     pid = fork();
     if (pid == -1) {
         perror("fork");
         exit(1);
-    }
+    } else if (pid == 0) {
+        // Child process
+        close(fd[1]);  // Close write end of pipe
 
-    if (pid == 0) {
-        const char* message = "Hello, parent process!";
-        write(pipefd[1], message, strlen(message) + 1); // Write the message to the pipe
-        close(pipefd[1]); // Close the write end of the pipe
-        child_process(pipefd[0], pipefd[1]);
+        // Run child process function
+        child_process(fd[0]);
+
+        // Should not reach here
+        exit(1);
     } else {
         // Parent process
-        read(pipefd[0], buffer, 1024); // Read the message from the pipe
-        printf("Received message from child process: %s\n", buffer);
+        close(fd[0]);  // Close read end of pipe
 
-        close(pipefd[0]); // Close the read end of the pipe
+        // Send commands to child process
+        write(fd[1], "print_hello\n", strlen("print_hello\n"));
+        write(fd[1], "exit\n", strlen("exit\n"));
 
-        // Send the function pointer to the child process through the pipe
-        void (*func)(void) = &run_server;
-        write(pipefd[1], &func, sizeof(func));
-
-        // Wait for the child process to exit
+        // Wait for child process to exit
         wait(NULL);
-    }
 
-    return 0;
+        // Close write end of pipe
+        close(fd[1]);
+
+        exit(0);
+    }
 }
