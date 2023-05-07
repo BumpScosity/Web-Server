b4d79de84762a92787a60916e829b9308e4a2bed
BumpScosity <BumpScosity@outlook.com>
2023-05-07 07:03:19 +0300

    Updates.

--- Main.c
+++ Main.c
@@ -1,4 +1,5 @@
 #include "lib/Main.h"
+#include <stdio.h>
 
 int main(int argc, char** argv) {
     pid_t pid;
@@ -9,13 +10,26 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
         // child process
+        printf("1");
         run_server();
+        printf("2");
         exit(EXIT_SUCCESS);
+        printf("3");
     } else {
         // parent process
         printf("Child process ID: %d\n", pid);
-
-        // wait for child process to exit
+        char command[100];
+        while(1) {
+            printf("(server) ");
+            fgets(command, sizeof(command), stdin);
+            command[strlen(command) - 1] = '\0';  // remove newline character
+            
+            if (strcmp(command, "stop") == 0) {
+                break;
+            } else {
+                printf("Unknown command\n");
+            }
+        }
         int status;
         waitpid(pid, &status, 0);
         printf("Child process exited with status %d\n", status);
