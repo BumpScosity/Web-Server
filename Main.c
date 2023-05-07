82a9afa6ba9efc8fe5993fe6c17ba6f47256621c
BumpScosity <BumpScosity@outlook.com>
2023-05-07 16:35:32 +0300

    Updates.

--- Main.c
+++ Main.c
@@ -1,22 +1,50 @@
 #include "lib/Main.h"
 
+void child_process(int read_pipe, int write_pipe) {
+    // Read the function pointer from the pipe
+    void (*func)(void);
+    read(read_pipe, &func, sizeof(func));
+
+    // Call the function
+    run_server();
+
+    // Close the pipes and exit
+    close(read_pipe);
+    close(write_pipe);
+    exit(0);
+}
+
 int main() {
+    int fd[2];
     pid_t pid;
 
+    // Create a pipe for communication between the parent and child processes
+    if (pipe(fd) == -1) {
+        perror("pipe");
+        exit(1);
+    }
+
+    // Fork the process
     pid = fork();
     if (pid == -1) {
-        perror("fork error");
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        // child process
-        run_server();
-        exit(EXIT_SUCCESS);
+        perror("fork");
+        exit(1);
+    }
+
+    if (pid == 0) {
+        // Child process
+        close(fd[1]); // Close the write end of the pipe
+        child_process(fd[0], fd[1]);
     } else {
-        // parent process
-        printf("Child process PID: %d\n", pid);
-        // parent process code goes here
-        // ...
-        wait(NULL); // wait for child process to terminate
+        // Parent process
+        close(fd[0]); // Close the read end of the pipe
+
+        // Send the function pointer to the child process through the pipe
+        void (*func)(void) = &run_server;
+        write(fd[1], &func, sizeof(func));
+
+        // Wait for the child process to exit
+        wait(NULL);
     }
 
     return 0;
