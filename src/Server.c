195f9136756ccb34a5e649e4d373f069bc807cff
BumpScosity <BumpScosity@outlook.com>
2023-05-09 19:23:42 +0300

    Updates.

--- src/Server.c
+++ src/Server.c
@@ -1,11 +1,53 @@
 #include "../lib/Main.h"
 
 void run_server(struct data *data) {
-    while (1) {
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html><body><h1>Hello, World!</h1></body></html>";
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1)
+    {   
         if (data->stop == 1) {
-            data->running = 0;
             data->stop = 0;
-            break;
+            data->running = 0;
+            exit(0);
         }
+        else if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        {
+            perror("accept failed");
+            exit(EXIT_FAILURE);
+        }
+
+        valread = read(new_socket, buffer, 1024);
+        printf("%s\n", buffer);
+
+        write(new_socket, hello, strlen(hello));
+        printf("Hello message sent\n");
+
+        close(new_socket);
     }
 }
