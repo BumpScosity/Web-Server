5430b29230cd26a7856269d1c7b3796dff15714b
BumpScosity <BumpScosity@outlook.com>
2023-05-09 19:11:37 +0300

    Updates.

--- src/Server.c
+++ src/Server.c
@@ -1,5 +1,11 @@
 #include "../lib/Main.h"
 
 void run_server(struct data *data) {
-    printf("Test");
+    while (1) {
+        if (data->stop == 1) {
+            data->running = 0;
+            data->stop = 0;
+            break;
+        }
+    }
 }
