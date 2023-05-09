55f1826272f55cffcbc50d3cf0e8b3d6f82bb8bc
BumpScosity <BumpScosity@outlook.com>
2023-05-09 19:12:25 +0300

    Updates.

--- src/Input.c
+++ src/Input.c
@@ -8,6 +8,10 @@ void input(struct data *data, char command[1024]) {
             data->running = 1;
             break;
         }
+        else if (strcmp(command, "stop\n") == 0) {
+            data->stop = 1;
+            break;
+        }
         else {
             printf("Unknown command, use \"To get a list of commands\"");
             break;
